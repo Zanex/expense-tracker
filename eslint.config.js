43ee9b14418b209d18b050d835d553ce6c653ea0
Alessio Zanella <alessio.zanella88@gmail.com>
2026-04-02 10:12:36 +0200

    chore: configure eslint with next/core-web-vitals and ignore .next directory

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,21 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  globalIgnores([".next/"]),
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: {...globals.browser, ...globals.node} } 
+  },
   tseslint.configs.recommended,
   ...(pluginReact.configs.flat.recommended ? [{
     ...pluginReact.configs.flat.recommended,
@@ -15,6 +26,7 @@ export default defineConfig([
       }
     }
   }] : []),
+  ...compat.extends("next/core-web-vitals"),
   {
     rules: {
       "react/react-in-jsx-scope": "off",
